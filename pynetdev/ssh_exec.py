894743cc7384919ef326cfe9c33e826afdda03d0
Gary Wright <zerosignal0@msn.com>
2014-07-01 21:13:59 -0700

    trying out env invoke within pynetdev.ssh_exec

--- pynetdev/ssh_exec.py
+++ pynetdev/ssh_exec.py
@@ -3,8 +3,7 @@
 Execution classes for ssh based connections, using
 fabric configuration.
 """
-from fabric.api import run, parallel, serial, execute
-import logging
+from fabric.api import env, run, parallel, serial, execute
 
 ###
 # Custom exception
@@ -23,19 +22,25 @@ class ssh_execute(object):
 
     @parallel
     def parallel_run_cmd(self):
+        env = self.env_settings
         for command in self.env.commands:
             results = run (command)
             if results:
-                logging.warning('command executed successfully, {}'.format(results))
+                self.logger.warning(
+                    'command executed successfully, {}'.format(results))
 
     @serial
     def serial_run_cmd(self):
+        env = self.env_settings
         for command in self.env.commands:
             results = run (command)
             if results:
-                logging.warning('command executed successfully, {}'.format(results))
+                self.logger.warning(
+                    'command executed successfully, {}'.format(results))
 
     def run_tests(self):
+        env = self.env_settings
+
         if self.env.parallel:
             execute(self.parallel_run_cmd())
         else:
