6ae4d65d79668eaf19cc0240793fc8c89aef877e
Gary Wright <zerosignal0@msn.com>
2014-07-02 12:19:53 -0700

    trying out env invoke within pynetdev.ssh_exec

--- pynetdev/ssh_exec.py
+++ pynetdev/ssh_exec.py
@@ -18,23 +18,16 @@ class ssh_execute(object):
     """
     def __init__(self, env_settings, logger):
 
-        print 'in ssh_execute __init__, setting up env'
-
         self.env = env_settings
         self.logger = logger
 
-        #env.hosts = self.env.hosts
-        #env.password = self.env.password
-        #env.user = self.env.user
-        #env.parallel = self.env.parallel
-        #env.abort_on_prompts = self.env.abort_on_prompts
 
     @parallel
-    def task_1(self):
+    def exec_task(self):
         for command in env.commands:
             results = run (str(command))
             if results:
-                logging.warning('command executed successfully, {}'.format(results))
+                self.logger.warning('command executed successfully, {}'.format(results))
 
     def run_tests(self):
-        execute(self.task_1)
+        execute(self.exec_task)
